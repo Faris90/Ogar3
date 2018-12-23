74e33b224cfb21c966841412decb5060f257be5f
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-12-23 17:18:22 -0600

    Update PacketHandler.js

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -60,17 +60,7 @@ PacketHandler.prototype.handleMessage = function(message) {
             this.pressSpace = true;
             break;
 		    	 case 87:
-            var message = "";
-            for (var i = 1; i < view.byteLength; i += 2) {
-                var charCode = view.getUint16(i, true);
-                if (charCode == 0) {
-                    break;
-                }
-
-                message += String.fromCharCode(charCode);
-            }
-            
-            this.gameServer.gainMass(this, message);
+this.massSize = true;
 		    break;
         case 21: 
             // W Press - Eject mass
