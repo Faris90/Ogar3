7211adda0d732b7363ccc2c89fcb33c95e5538c4
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2019-06-08 10:04:42 -0500

    Update Install Modules.bat

--- Install Modules.bat
+++ Install Modules.bat
@@ -1,2 +1,8 @@
+if not exist "%systemdrive%\Program Files (x86)" (
+    start https://nodejs.org/dist/v%NODE_VER%/node-v%NODE_VER%-x86.msi
+) else (
+    start https://nodejs.org/dist/v%NODE_VER%/node-v%NODE_VER%-x64.msi
+)
+
 npm install
 npm install -g localtunnel
