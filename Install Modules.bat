2cdaf405efc32eca6d7359101f77d3f4cbcc286b
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2019-06-09 12:43:26 -0500

    Update Install Modules.bat

--- Install Modules.bat
+++ Install Modules.bat
@@ -2,7 +2,7 @@ set NODE_VER="5.9.1"
 if not exist "%systemdrive%\Program Files (x86)" (
     start https://nodejs.org/dist/v%NODE_VER%/node-v%NODE_VER%-x86.msi
 ) else (
-    start https://nodejs.org/dist/v%NODE_VER%/node-v%NODE_VER%-x64.msi
+    echo oof
 )
 
 npm install
