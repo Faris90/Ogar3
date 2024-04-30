e96113bf432ff2799c8084aa23e2adaada3194eb
Emupedia <admin@emupedia.net>
2024-04-30 20:36:55 +0300

    More work

--- docs/main.js
+++ docs/main.js
@@ -624,15 +624,15 @@
 		if (from < 0) from = 0;
 		for (var i = 0; i < (len - from); i++) {
 			var chatName = new UText(18, chatBoard[i + from].color);
-			chatName.setValue(chatBoard[i + from].name + ': ');
+			chatName.setValue(chatBoard[i + from].name);
 			var width = chatName.getWidth();
 			var a = chatName.render();
 			ctx.drawImage(a, 15, chatCanvas.height / scaleFactor - 24 * (len - i - from));
 
 			var chatText = new UText(18, '#666666');
-			chatText.setValue(chatBoard[i + from].message);
+			chatText.setValue(': ' + chatBoard[i + from].message);
 			a = chatText.render();
-			ctx.drawImage(a, 15 + width * 1.8, chatCanvas.height / scaleFactor - 24 * (len - from - i));
+			ctx.drawImage(a, 3 + width, chatCanvas.height / scaleFactor - 24 * (len - from - i));
 		}
 		//ctx.restore();
 	}
