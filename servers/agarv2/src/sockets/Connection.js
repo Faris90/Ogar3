86211838988c11d1362dcea68d6b394e2c39c146
Emupedia <admin@emupedia.net>
2024-05-11 03:28:05 +0300

    More work

--- servers/agarv2/src/sockets/Connection.js
+++ servers/agarv2/src/sockets/Connection.js
@@ -3,183 +3,228 @@ const Reader = require("../primitives/Reader");
 const { filterIPAddress } = require("../primitives/Misc");
 
 class Connection extends Router {
-    /**
-     * @param {Listener} listener
-     * @param {WebSocket} webSocket
-     */
-    constructor(listener, webSocket, req) {
-        super(listener);
-        const ip = typeof req.headers['x-real-ip'] !== 'undefined' ? req.headers['x-real-ip'] : req.socket.remoteAddress;
-        this.remoteAddress = filterIPAddress(ip);
-        this.verifyScore = -1;
-        this.webSocket = webSocket;
-        this.req = req;
-        this.connectTime = Date.now();
-        this.lastActivityTime = Date.now();
-        this.lastChatTime = Date.now();
+	/**
+	 * @param {Listener} listener
+	 * @param {WebSocket} webSocket
+	 */
+	constructor(listener, webSocket, req) {
+		super(listener);
+		const ip = typeof req.headers['x-real-ip'] !== 'undefined' ? req.headers['x-real-ip'] : req.socket.remoteAddress;
+		this.remoteAddress = filterIPAddress(ip);
+		this.verifyScore = -1;
+		this.webSocket = webSocket;
+		this.req = req;
+		this.connectTime = Date.now();
+		this.lastActivityTime = Date.now();
+		this.lastChatTime = Date.now();
 
-        this.upgradeLevel = 0;
-        /** @type {Protocol} */
-        this.protocol = null;
+		this.upgradeLevel = 0;
+		/** @type {Protocol} */
+		this.protocol = null;
 
-        this.socketDisconnected = false;
-        this.closeCode = NaN;
-        /** @type {string} */
-        this.closeReason = null;
+		this.socketDisconnected = false;
+		this.closeCode = NaN;
+		/** @type {string} */
+		this.closeReason = null;
 
-        /** @type {Minion[]} */
-        this.minions = [];
-        this.minionsFrozen = false;
-        this.controllingMinions = false;
+		/** @type {Minion[]} */
+		this.minions = [];
+		this.minionsFrozen = false;
+		this.controllingMinions = false;
 
-        webSocket.on("close", this.onSocketClose.bind(this));
-        webSocket.on("message", this.onSocketMessage.bind(this));
-        webSocket.on("ping", this.closeSocket.bind(this, 1003, "Unexpected message format"));
-        webSocket.on("pong", this.closeSocket.bind(this, 1003, "Unexpected message format"));
-    }
+		webSocket.on("close", this.onSocketClose.bind(this));
+		webSocket.on("message", this.onSocketMessage.bind(this));
+		webSocket.on("ping", this.closeSocket.bind(this, 1003, "Unexpected message format"));
+		webSocket.on("pong", this.closeSocket.bind(this, 1003, "Unexpected message format"));
+	}
 
-    close() {
-        if (!this.socketDisconnected) return void this.closeSocket(1001, "Manual connection close call");
-        super.close();
-        this.disconnected = true;
-        this.disconnectionTick = this.handle.tick;
-        this.listener.onDisconnection(this, this.closeCode, this.closeReason);
-        this.webSocket.removeAllListeners();
-    }
+	close() {
+		if (!this.socketDisconnected) return void this.closeSocket(1001, "Manual connection close call");
+		super.close();
+		this.disconnected = true;
+		this.disconnectionTick = this.handle.tick;
+		this.listener.onDisconnection(this, this.closeCode, this.closeReason);
+		this.webSocket.removeAllListeners();
+	}
 
-    static get type() { return "connection"; }
-    static get isExternal() { return true; }
-    static get separateInTeams() { return true; }
+	static get type() { return "connection"; }
+	static get isExternal() { return true; }
+	static get separateInTeams() { return true; }
 
-    /**
-     * @param {number} code
-     * @param {string} reason
-     */
-    onSocketClose(code, reason) {
-        if (this.socketDisconnected) return;
-        this.logger.debug(`connection from ${this.remoteAddress} has disconnected`);
-        this.socketDisconnected = true;
-        this.closeCode = code;
-        this.closeReason = reason;
-    }
+	/**
+	 * @param {number} code
+	 * @param {string} reason
+	 */
+	onSocketClose(code, reason) {
+		if (this.socketDisconnected) return;
+		this.logger.debug(`connection from ${this.remoteAddress} has disconnected`);
+		this.socketDisconnected = true;
+		this.closeCode = code;
+		this.closeReason = reason;
+	}
 
-    /**
-     * @param {ArrayBuffer|string} data
-     */
-    onSocketMessage(data) {
-        if (data instanceof String) return void this.closeSocket(1003, "Unexpected message format");
-        if (data.byteLength > 512 || data.byteLength === 0)
-            return void this.closeSocket(1009, "Unexpected message size");
-        this.lastActivityTime = Date.now();
-        const reader = new Reader(Buffer.from(data), 0);
-        if (this.protocol !== null) this.protocol.onSocketMessage(reader);
-        else {
-            this.protocol = this.handle.protocols.decide(this, reader);
-            if (this.protocol === null) return void this.closeSocket(1003, "Ambiguous protocol");
-        }
-    }
-    createPlayer() {
-        super.createPlayer();
-        if (this.settings.chatEnabled)
-            this.listener.globalChat.add(this);
-        if (this.settings.matchmakerNeedsQueuing) {
-            this.listener.globalChat.directMessage(null, this, "This server requires players to be queued.");
-            this.listener.globalChat.directMessage(null, this, "Try spawning to enqueue.");
-        } else
-            this.handle.matchmaker.toggleQueued(this);
-    }
+	/**
+	 * @param {ArrayBuffer|string} data
+	 */
+	onSocketMessage(data) {
+		if (data instanceof String) return void this.closeSocket(1003, "Unexpected message format");
+		if (data.byteLength > 512 || data.byteLength === 0)
+			return void this.closeSocket(1009, "Unexpected message size");
+		this.lastActivityTime = Date.now();
+		const reader = new Reader(Buffer.from(data), 0);
+		if (this.protocol !== null) this.protocol.onSocketMessage(reader);
+		else {
+			this.protocol = this.handle.protocols.decide(this, reader);
+			if (this.protocol === null) return void this.closeSocket(1003, "Ambiguous protocol");
+		}
+	}
 
-    /**
-     * @param {string} message
-     */
-    onChatMessage(message) {
-        message = message.trim();
-        if (!message) return;
-        const lastChatTime = this.lastChatTime;
-        this.lastChatTime = Date.now();
-        if (message.length >= 2 && message[0] === "/") {
-            if (!this.handle.chatCommands.execute(this, message.slice(1))) this.listener.globalChat.directMessage(null, this, "unknown command, execute /help for the list of commands");
-        } else {
-            if (Date.now() - lastChatTime >= this.settings.chatCooldown) {
-                 this.listener.globalChat.broadcast(this, message);
-            } else {
-                 this.listener.globalChat.directMessage(null, this, "Please don't spam.");
-            }
-        }
-    }
-    onQPress() {
-        if (!this.hasPlayer) return;
-        if (this.listener.settings.minionEnableQBasedControl && this.minions.length > 0)
-            this.controllingMinions = !this.controllingMinions;
-        else this.handle.gamemode.whenPlayerPressQ(this.player);
-    }
-    get shouldClose() {
-        return this.socketDisconnected;
-    }
-    update() {
-        if (!this.hasPlayer) return;
-        if (!this.player.hasWorld) {
-            if (this.spawningName !== null)
-                this.handle.matchmaker.toggleQueued(this);
-            this.spawningName = null;
-            this.splitAttempts = 0;
-            this.ejectAttempts = 0;
-            this.requestingSpectate = false;
-            this.isPressingQ = false;
-            this.hasProcessedQ = false;
-            return;
-        }
-        this.player.updateVisibleCells();
+	createPlayer() {
+		super.createPlayer();
+		if (this.settings.chatEnabled)
+			this.listener.globalChat.add(this);
+		if (this.settings.matchmakerNeedsQueuing) {
+			this.listener.globalChat.directMessage(null, this, "This server requires players to be queued.");
+			this.listener.globalChat.directMessage(null, this, "Try spawning to enqueue.");
+		} else
+			this.handle.matchmaker.toggleQueued(this);
+	}
 
-        const add = [], upd = [], eat = [], del = [];
-        const player = this.player;
-        const     visible = player.visibleCells,
-              lastVisible = player.lastVisibleCells;
-        for (let id in visible) {
-            const cell = visible[id];
-            if (!lastVisible.hasOwnProperty(id)) add.push(cell);
-            else if (cell.shouldUpdate) upd.push(cell);
-        }
-        for (let id in lastVisible) {
-            const cell = lastVisible[id];
-            if (visible.hasOwnProperty(id)) continue;
-            if (cell.eatenBy !== null) eat.push(cell);
-            del.push(cell);
-        }
+	/**
+	 * @param {string} message
+	 */
+	onChatMessage(message) {
+		message = message.trim();
 
-        if (player.state === 1 || player.state === 2)
-            this.protocol.onSpectatePosition(player.viewArea);
-        if (this.handle.tick % 4 === 0)
-            this.handle.gamemode.sendLeaderboard(this);
-        this.protocol.onVisibleCellUpdate(add, upd, eat, del);
-    }
-    onWorldSet() {
-        this.protocol.onNewWorldBounds(this.player.world.border, true, this.protocol);
-    }
-    /** @param {PlayerCell} cell */
-    onNewOwnedCell(cell) {
-        this.protocol.onNewOwnedCell(cell);
-    }
-    onWorldReset() {
-        this.protocol.onWorldReset();
-    }
+		if (!message) return;
 
-    /** @param {Buffer} data */
-    send(data) {
-        if (this.socketDisconnected) return;
-        this.webSocket.send(data);
-    }
-    /**
-     * @param {number=} code
-     * @param {string=} reason
-     */
-    closeSocket(code, reason) {
-        if (this.socketDisconnected) return;
-        this.socketDisconnected = true;
-        this.closeCode = code;
-        this.closeReason = reason;
-        this.webSocket.close(code || 1006, reason || "");
-    }
+		const lastChatTime = this.lastChatTime;
+		const lastMessage = this.lastMessage;
+		const lastlastMessage = this.lastlastMessage;
+		const lastlastlastMessage = this.lastlastlastMessage;
+
+		if (message[0] === "/" && message.length >= 2) {
+			if (!this.handle.chatCommands.execute(this, message.slice(1))) {
+				this.listener.globalChat.directMessage(null, this, "unknown command, execute /help for the list of commands");
+			}
+			return;
+		}
+
+		if (message.length <= 1) {
+			this.listener.globalChat.directMessage(null, this, "[AntiSpam] Your message is too short, write longer messages.");
+			return;
+		}
+
+		if (message.length >= 10 && !~message.indexOf(' ')) {
+			this.listener.globalChat.directMessage(null, this, "[AntiSpam] Your message doesn't has any spaces, add some spaces.");
+			return;
+		}
+
+		if (!lastChatTime || (Date.now() - lastChatTime >= this.settings.chatCooldown)) {
+			if (lastMessage) {
+				if (lastMessage === message || (~lastMessage.indexOf(message) || ~message.indexOf(lastMessage)) && message.length >= 10) {
+					this.listener.globalChat.directMessage(null, this, "[AntiSpam] Please don't repeat yourself, write something different.");
+					return;
+				}
+			}
+
+			if (lastlastMessage) {
+				if (lastlastMessage === message || (~lastlastMessage.indexOf(message) || ~message.indexOf(lastlastMessage)) && message.length >= 10) {
+					this.listener.globalChat.directMessage(null, this, "[AntiSpam] Please don't repeat yourself, write something different.");
+					return;
+				}
+			}
+
+			if (lastlastlastMessage) {
+				if (lastlastlastMessage === message || (~lastlastlastMessage.indexOf(message) || ~message.indexOf(lastlastlastMessage)) && message.length >= 10) {
+					this.listener.globalChat.directMessage(null, this, "[AntiSpam] Please don't repeat yourself, write something different.");
+					return;
+				}
+			}
+
+			this.listener.globalChat.broadcast(this, message);
+
+			this.lastChatTime = Date.now();
+			this.lastlastlastMessage = lastlastMessage;
+			this.lastlastMessage = lastMessage;
+			this.lastMessage = message;
+		} else {
+			this.listener.globalChat.directMessage(null, this, "[AntiSpam] Please don't write too fast, wait at least " + (this.settings.chatCooldown / 1000)  + " seconds.");
+		}
+	}
+	onQPress() {
+		if (!this.hasPlayer) return;
+		if (this.listener.settings.minionEnableQBasedControl && this.minions.length > 0)
+			this.controllingMinions = !this.controllingMinions;
+		else this.handle.gamemode.whenPlayerPressQ(this.player);
+	}
+	get shouldClose() {
+		return this.socketDisconnected;
+	}
+	update() {
+		if (!this.hasPlayer) return;
+		if (!this.player.hasWorld) {
+			if (this.spawningName !== null)
+				this.handle.matchmaker.toggleQueued(this);
+			this.spawningName = null;
+			this.splitAttempts = 0;
+			this.ejectAttempts = 0;
+			this.requestingSpectate = false;
+			this.isPressingQ = false;
+			this.hasProcessedQ = false;
+			return;
+		}
+		this.player.updateVisibleCells();
+
+		const add = [], upd = [], eat = [], del = [];
+		const player = this.player;
+		const     visible = player.visibleCells,
+			lastVisible = player.lastVisibleCells;
+		for (let id in visible) {
+			const cell = visible[id];
+			if (!lastVisible.hasOwnProperty(id)) add.push(cell);
+			else if (cell.shouldUpdate) upd.push(cell);
+		}
+		for (let id in lastVisible) {
+			const cell = lastVisible[id];
+			if (visible.hasOwnProperty(id)) continue;
+			if (cell.eatenBy !== null) eat.push(cell);
+			del.push(cell);
+		}
+
+		if (player.state === 1 || player.state === 2)
+			this.protocol.onSpectatePosition(player.viewArea);
+		if (this.handle.tick % 4 === 0)
+			this.handle.gamemode.sendLeaderboard(this);
+		this.protocol.onVisibleCellUpdate(add, upd, eat, del);
+	}
+	onWorldSet() {
+		this.protocol.onNewWorldBounds(this.player.world.border, true, this.protocol);
+	}
+	/** @param {PlayerCell} cell */
+	onNewOwnedCell(cell) {
+		this.protocol.onNewOwnedCell(cell);
+	}
+	onWorldReset() {
+		this.protocol.onWorldReset();
+	}
+
+	/** @param {Buffer} data */
+	send(data) {
+		if (this.socketDisconnected) return;
+		this.webSocket.send(data);
+	}
+	/**
+	 * @param {number=} code
+	 * @param {string=} reason
+	 */
+	closeSocket(code, reason) {
+		if (this.socketDisconnected) return;
+		this.socketDisconnected = true;
+		this.closeCode = code;
+		this.closeReason = reason;
+		this.webSocket.close(code || 1006, reason || "");
+	}
 }
 
 module.exports = Connection;
